12aed453bfe877d9a50d7f67b5d82014b23da627
alenamandrika <Alenenok0@ukr.net>
2018-03-05 23:18:22 +0200

    deleted folder backend

--- backend/server/auth.js
+++
@@ -1,6 +0,0 @@
-export const facebook = {
-  clientID: '1839663726067663',
-  clientSecret: '02ea0734e86e706ed1128bf370f18820',
-  callbackURL: 'http://localhost:3005/auth/facebook/callback',
-  profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
-};
--- backend/server/config.js
+++
@@ -1,6 +0,0 @@
-const host = 'localhost'
-
-module.exports = {
-  port: process.env.port || process.env.PORT || '3005',
-  mongoUrl: process.env.MONGODB_URI || `mongodb://username:123@ds255588.mlab.com:55588/users`
-}
